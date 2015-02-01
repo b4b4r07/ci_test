c634194505349f63dda0bd557738bb6a76a36f05
b4b4r07 <b4b4r07@gmail.com>
2015-02-02 00:59:01 +0900

    test antigen;

---
+++ antigen.pl
@@ -0,0 +1,35 @@
+#!/usr/bin/perl
+
+use strict;
+use warnings;
+use FindBin;
+use Cwd;
+use Test::More;
+
+#my $root = Cwd::abs_path($FindBin::Bin . "/../");
+#like "$root", qr/dotfiles\/(?:(?!\/).)*$/, 'within dotfiles/etc';
+#
+#my @list = glob "$root/init/{,osx/}*.sh";
+#cmp_ok @list, '>', 0, '@list > 0';
+#
+#foreach my $f (@list) {
+#    # Check debug mode init scripts
+#    system("DEBUG=1 bash $f >/dev/null");
+#
+#    # Shorten path
+#    #$f =~ s/^.*dotfiles\///;
+#    #ok $? == 0, sub{my $a = shift; $a =~ s/^.*dotfiles\///; $a}->($f);
+#    #ok $? == 0, sub{s/^.*dotfiles\///; $_}->($f);
+#    ok $? == 0, sub($) {
+#        my $v = shift;
+#        $v =~ s/^.*dotfiles\///; $v;
+#    }->($f);
+#}
+
+system("git clone https://github.com/zsh-users/antigen ~/.antigen");
+
+ok -d "$ENV{'HOME'}/.antigen";
+system("zsh; antigen help");
+ok $? == 0;
+
+done_testing;
