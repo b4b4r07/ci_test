9f62fc31b1320150f827de7f270e609fa834b76a
b4b4r07 <b4b4r07@gmail.com>
2015-02-01 20:13:15 +0900

    first commit

---
+++ test.pl
@@ -0,0 +1,17 @@
+#!/usr/bin/perl
+
+use strict;
+use warnings;
+use FindBin;
+use Cwd;
+use Test::More;
+
+my @list = glob getcwd . "/*.sh";
+cmp_ok @list, '>', 0, '@list > 0';
+
+foreach my $f (@list) {
+    # Check debug mode init scripts
+    system("bash $f >/dev/null");
+    ok $? == 0;
+}
+done_testing;
